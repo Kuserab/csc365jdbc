4046a988b80469a56738373f813468b0fe80b0da
Hunter-Lane <38011818+Hunter-Lane@users.noreply.github.com>
2020-03-16 14:30:27 -0700

    Added wildcard in compile

--- shell_run.sh
+++ shell_run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-javac jdbc/src/InnReservations.java jdbc/src/Room.java
-
 export CLASSPATH=$CLASSPATH:mysql-connector-java-8.0.16.jar:.
 export APP_JDBC_URL=jdbc:mysql://db.labthreesixfive.com/egarc113?autoReconnect=true
 
@@ -11,5 +9,6 @@ export APP_JDBC_USER=$uservar
 export APP_JDBC_PW=WinterTwenty20_365_$passvar
 
 cd jdbc/src
+javac *.java
 java -cp mysql-connector-java-8.0.16.jar:. InnReservations
 cd -
